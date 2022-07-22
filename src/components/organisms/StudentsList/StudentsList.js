4af34169a8b1592354692005f2d0526c66797a15
Adam Jasinski <admjasinski@gmail.com>
2022-07-22 16:31:23 +0200

    Add routing and handle events functionality

--- src/components/organisms/StudentsList/StudentsList.js
+++ src/components/organisms/StudentsList/StudentsList.js
@@ -1,28 +1,11 @@
-import { students as studentData } from 'data/students';
 import StudentsListItem from 'components/molecules/StudentsListItem/StudentsListItem';
 import { Wrapper, StyledList, StyledTitle } from './StudentsList.styles';
-import { useState } from 'react';
-import FormField from 'components/molecules/FormField/FormField';
-import { Button } from 'components/atoms/Button/Button';
 
-const StudentsList = () => {
-  const [students, setStudents] = useState(studentData);
-
-  const deleteStudent = (name) => {
-    const filteredStudents = students.filter((student) => student.name !== name);
-    setStudents(filteredStudents);
-  };
-
+const StudentsList = ({ students, deleteStudent }) => {
   return (
     <>
       <Wrapper>
-        <StyledTitle>Add new student</StyledTitle>
-        <FormField label="Name" id="name" name="name" />
-        <FormField label="Attendence" id="name" name="name" />
-        <FormField label="Average" id="name" name="name" />
-        <Button>Add</Button>
-      </Wrapper>
-      <Wrapper>
+        <StyledTitle>Students list</StyledTitle>
         <StyledList>
           {students.map((studentData) => (
             <StudentsListItem deleteStudent={deleteStudent} key={studentData.name} studentData={studentData} />
